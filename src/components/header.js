267bc64dbcde6c7ad3d610dee39c64e4470d999b
oddEvenAndroid <oddevenandriod@gmail.com>
2019-07-08 19:28:32 +0530

    header line draw

--- src/components/header.js
+++ src/components/header.js
@@ -20,10 +20,15 @@ const styles = {
         backgroundColor:'#F8F8F8',
         height:60,
         alignItems:'center',
-        paddingTop:15
+        paddingTop:10,
+        shadowColor:'#000',
+        shadowOffset:{ width: 0, height: 2 },
+        shadowOpacity:0.2,
+        elevation: 2,
+        postion: 'relative'
     },
     textStyle:{
-        fontSize: 30
+        fontSize: 20
     }
 };
 
